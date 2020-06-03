aebf5766bbbadb243ab83306dadc850fac005cef
mmorito <morito@mnes.org>
2020-06-04 00:59:37 +0900

    add: firebase configs

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyC3ad6VmChU83TYQgvQa5K9RP73NoWWHZw',
+    authDomain: 'mmorito-leo.firebaseapp.com',
+    databaseURL: 'https://mmorito-leo.firebaseio.com',
+    projectId: 'mmorito-leo',
+    storageBucket: 'mmorito-leo.appspot.com',
+    messagingSenderId: '302392904357',
+    appId: '1:302392904357:web:d28d1d0541ee7daaf69b4c',
+    measurementId: 'G-JW55RMLE8H'
+  }
 };
